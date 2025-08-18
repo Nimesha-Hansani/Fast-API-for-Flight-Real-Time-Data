6d91c9a0f7806bea7b0b961b17696926a9fbeb7a
Nimesha Hansani Amarasinghe <nimesha@3rivetech.com>
2025-08-18 13:34:14 +0530

    Main Py changed for get flight data

--- main.py
+++ main.py
@@ -3,13 +3,38 @@ import time
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
 import uvicorn
+import requests
+
+
+API_KEY = 'b9a99cfbc2971adba7d9b72b3264a66d'  # Use env variable in App Runner
+API_URL = "http://api.aviationstack.com/v1/flights"
+# OFFSET_FILE = "offset.json"
+LIMIT = 5
+# FETCH_INTERVAL = 10 
 
 latest_flights = []
 
+def fetch_flights():
+
+    params = {
+        "access_key": API_KEY,
+        "limit": LIMIT
+    }
+     
+    response = requests.get(API_URL, params=params)
+    response.raise_for_status()
+    data = response.json()
+    
+    data = response.json()
+    return data.get("data", [])
+
+
 def background_fetch():
-    while True:
-        latest_flights.append({"flight": "EK123", "status": "On Time"})
-        time.sleep(10)
+
+    flights = fetch_flights()
+    latest_flights.extend(flights)
+    
+
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
